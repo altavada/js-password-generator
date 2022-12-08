04f799a0acb57107c6837e2715f3bb4ea183ebd7
altavada <117209509+altavada@users.noreply.github.com>
2022-12-07 22:52:45 -0500

    created random character generator

--- Develop/script.js
+++ Develop/script.js
@@ -7,10 +7,35 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
+// Password criteria
+var includeLowercase = false;
+var includeUppercase = false;
+var includeNumbers = false;
+var includeCharacters = false;
+
 // Random-number generator
 function roll(max) {
     return Math.floor(Math.random() * max);
 }
+
+// Character randomizer
+function typeSelector() {
+    let characterType = roll(4);
+    if (!includeLowercase && !includeUppercase && !includeNumbers && !includeCharacters) {
+        return ("Type selection error.");
+      } else if (characterType == 0 && includeLowercase) {
+        return lowercase[roll(26)];
+      } else if (characterType == 1 && includeUppercase) {
+        return uppercase[roll(26)];
+      } else if (characterType == 2 && includeNumbers) {
+        return numbers[roll(10)];
+      } else if (characterType == 3 && includeCharacters) {
+        return characters[roll(10)];
+      } else {
+        return typeSelector();
+    }
+}
+  
 
 // Write password to the #password input, resets values.
 function writePassword() {
@@ -18,7 +43,7 @@ function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
-  }
+}
   
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
