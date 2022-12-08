4d1a021e6aaf878332034d05eb9e0257abdd9f9f
altavada <117209509+altavada@users.noreply.github.com>
2022-12-07 22:27:03 -0500

    initial files plus starter code and a few initial js edits beginning to set global variables and tweaking the writePassword function

---
+++ Develop/script.js
@@ -0,0 +1,19 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Character arrays
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
+// Write password to the #password input, resets values.
+function writePassword() {
+    passwordArray = [];
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+  
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
